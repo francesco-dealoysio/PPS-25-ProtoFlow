95fbb82e710a28ea5bfece21cbff38183a127ab8
Francesco de Aloysio <francesco.dealoysio@studio.unibo.it>
2026-07-03 02:45:05 +0200

    Added dummy HomepageAdmin mask

---
+++ src/main/scala/pkg/a/gui/HomepageAdmin.scala
@@ -0,0 +1,27 @@
+package pkg.a.gui
+
+import java.awt.Toolkit
+import scala.swing.{BorderPanel, Button, Dimension, Label, MainFrame}
+import scala.swing.event.ButtonClicked
+
+class HomepageAdmin extends MainFrame {
+  title = "Administrator Homepage"
+  contents = new Label("Hello from App 2!")
+  val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+  preferredSize = screenSize
+
+
+  val label = new Label("Hello from Administrator Homepage!")
+  val closeButton = new Button("Close")
+
+  contents = new BorderPanel {
+    layout(label) = BorderPanel.Position.Center
+    layout(closeButton) = BorderPanel.Position.South
+  }
+
+  listenTo(closeButton)
+  reactions += {
+    case ButtonClicked(`closeButton`) =>
+      this.close() // Close only this window
+  }
+}
