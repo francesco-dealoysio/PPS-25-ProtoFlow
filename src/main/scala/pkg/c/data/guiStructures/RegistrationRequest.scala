643b60d57325cc11032064b0d82afc0638ffc712
Roberto Pisu <roberto.pisu03@gmail.com>
2026-07-08 10:31:23 +0200

    Created first draft of the registration gui

---
+++ src/main/scala/pkg/c/data/guiStructures/RegistrationRequest.scala
@@ -0,0 +1,11 @@
+package pkg.c.data.guiStructures
+
+case class RegistrationRequest(
+                                name: String,
+                                surname: String,
+                                email: String,
+                                phone: String,
+                                requestedRole: String,
+                                area: String,
+                                assignment: String
+                              )
